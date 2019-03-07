ae04b11cec9b03fc03230372facd93d4c4670099
杨汉春 <18819424827@163.com>
2019-03-07 15:45:54 +0800

    添加测试文件

--- eschool-wechat-backend/src/main/java/com/eschool/SpringCloudApplication.java
+++ eschool-wechat-backend/src/main/java/com/eschool/SpringCloudApplication.java
@@ -1,9 +1,11 @@
 package com.eschool;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.eschool")
+@MapperScan({"com.eschool.**.mapper"})
 public class SpringCloudApplication {
 
 	public static void main(String[] args) {
---
+++ eschool-wechat-backend/src/main/java/com/eschool/code/index/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.eschool.code.index.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yanghanchun
+ * @date 2019/3/7 15:35
+ * @Description:
+ */
+@RestController
+@RequestMapping("/api/index")
+public class IndexController {
+
+    @GetMapping("/list")
+    public String list(@RequestParam(required = false) String title){
+        return "标题：" + title;
+    }
+}
