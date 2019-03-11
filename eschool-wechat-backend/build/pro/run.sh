38b5895e058c39351ea906a1cef65616780c622a
杨汉春 <18819424827@163.com>
2019-03-11 15:27:40 +0800

    添加docker+jenkins自动化部署测试配置

---
+++ eschool-wechat-backend/build/pro/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd /root/dockerfile/eschool-wechat-backend
+# 使用当前目录下的dockerfile构建镜像
+docker build -t yhc/eschool:pro .
+# 删除原来的容器
+aa=$(docker ps -a|grep eschool|awk '{print $1}')
+if [ "$aa" != "" ] ; then
+echo delete $aa ...
+docker rm -f $aa
+fi
+echo run new CONTAINER ...
+# 启动容器
+docker run -v /etc/timezone:/etc/timezone -v /etc/localtime:/etc/localtime -v /data/logs/eschool-wechat-backend:/logs -d --name eschool -p 80:80 --restart=always yhc/eschool:pro
